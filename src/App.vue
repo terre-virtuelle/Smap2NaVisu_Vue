ae245f950fd8d6372654ab2df7f55030fca6aefc
niktopelo <niktopelo@gmail.com>
2022-01-19 15:24:17 +0100

    commit init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <FormDisplay/>
+</template>
+
+<script>
+import FormDisplay from './components/FormDisplay.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormDisplay
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FormDisplay.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="editor_holder">
+      </div>
+</template>
+
+<script>
+import schemaCN from "../assets/schemaCN.json";
+import {JSONEditor} from "@json-editor/json-editor";
+import { onMounted} from "vue";
+
+export default {
+  name: "FormDisplay",
+  // this component is written in the vuejs 3 way
+  setup() {
+    var editor = null
+    // we use onMonter because setup() apend to early in the vue lifecycle
+    onMounted(() => {
+      const editor_holder = document.getElementById('editor_holder');
+      // we ccan add another attributes in the options to add styles for example
+      editor = new JSONEditor(editor_holder, {schema:schemaCN});
+    })
+    return {
+      editor
+    }
+
+  }
+}
+</script>
+
